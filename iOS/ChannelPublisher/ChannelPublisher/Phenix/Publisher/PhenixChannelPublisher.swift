75b292eba2f2b804624252758a63e8fe000dc7a7
vova.gorbenko <vova.gorbenko@phenixrts.com>
2023-06-22 10:44:41 +0200

    [iOS] [ChannelPublisher] Add Refresh Authentication Token example (#APP-1043) Fixed
    - Added method to observe an Authentication status
    
    Change-Id: Iafc05e69ae7770170b414dd97c2f46f2b1b1736b
    Reviewed-on: https://gerrit.phenixrts.com/c/MobileChannelPublisher/+/15962
    Reviewed-by: Vova Gorbenko <vova.gorbenko@phenixrts.com>
    Reviewed-by: Cyril Caillaba <cyril.caillaba@phenixrts.com>
    Reviewed-by: Ron Bakker <ron.bakker@phenixrts.com>
    Reviewed-by: Andreas Schuler <andreas.schuler@phenixrts.com>
    Reviewed-by: Tim Gronkowski <tim.gronkowski@phenixrts.com>
    Tested-by: Vova Gorbenko <vova.gorbenko@phenixrts.com>

--- iOS/ChannelPublisher/ChannelPublisher/Phenix/Publisher/PhenixChannelPublisher.swift
+++ iOS/ChannelPublisher/ChannelPublisher/Phenix/Publisher/PhenixChannelPublisher.swift
@@ -10,8 +10,30 @@ public final class PhenixChannelPublisher {
 
     public weak var delegate: PhenixChannelPublisherDelegate?
 
+    private var authenticationStatusDisposable: PhenixDisposable?
+
     public init(channelExpress: PhenixChannelExpress) {
         self.channelExpress = channelExpress
+        self.observeAuthenticationStatus()
+    }
+
+    private func observeAuthenticationStatus() {
+        authenticationStatusDisposable = channelExpress.pcastExpress.getObservableAuthenticationStatus().subscribe { [weak self] change in
+            guard let self = self else { return }
+            let status = PhenixAuthenticationStatus(rawValue: change!.value.intValue)!
+
+            if PhenixAuthenticationStatus.unauthenticated == status {
+                self.handleUnauthenticatedStatus()
+            }
+        }
+    }
+
+    private func handleUnauthenticatedStatus() {
+        // You can fetch a new authentication token and set it using setAuthenticationToken method to re-authenticate the SDK
+        AppDelegate.terminate(
+            afterDisplayingAlertWithTitle: "Failed to authenticate the Phenix SDK. The provided authentication token has expired",
+            message: "Application will be terminated."
+        )
     }
 
     public func publish(userMediaStream: PhenixUserMediaStream) {
